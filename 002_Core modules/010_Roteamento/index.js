5aac5a6dd44e12de546246fb4963fa0a73d04ab8
wesleyara <wes1gom@gmail.com>
2022-04-06 10:57:38 -0300

    feat: Adicionando pasta 011

---
+++ 002_Core modules/010_Roteamento/index.js
@@ -0,0 +1,29 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const port = 3000;
+
+http
+  .createServer((req, res) => {
+    const q = url.parse(req.url, true);
+    const fileName = q.pathname.substring(1);
+    if (fileName.includes("html")) {
+      if (fs.existsSync(fileName)) {
+        fs.readFile(fileName, (err, data) => {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          return res.end();
+        });
+      } else {
+        fs.readFile("404.html", (err, data) => {
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.write(data);
+          return res.end();
+        });
+      }
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
