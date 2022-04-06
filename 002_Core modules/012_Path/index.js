df44cdb6836d5489cb08937e0bb73e74a9552e6b
wesleyara <wes1gom@gmail.com>
2022-04-06 11:00:12 -0300

    feat: Adicionando pasta 012

---
+++ 002_Core modules/012_Path/index.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+const arquivo = "/home/user/wesley/documents/arquivo.txt";
+
+console.log(path.dirname(arquivo));
+console.log(path.basename(arquivo));
+console.log(path.extname(arquivo));
