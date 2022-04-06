495848a0ee427013ae1c3c86be5370c24fd9166c
wesleyara <wes1gom@gmail.com>
2022-04-06 10:52:17 -0300

    feat: Adicionando pasta 007

---
+++ 002_Core modules/007_Mesclando/index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+
+const port = 3000;
+
+http
+  .createServer((req, res) => {
+    const urlInfo = url.parse(req.url, true);
+    const name = urlInfo.query.name;
+
+    if (!name) {
+      fs.readFile("index.html", (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      const newLine = name + ",\r\n";
+      fs.appendFile("arquivo.txt", newLine, () => {
+        res.writeHead(302, { Location: "/" });
+        return res.end();
+      });
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
