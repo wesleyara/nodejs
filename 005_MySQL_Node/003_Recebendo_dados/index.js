c770831d9b1b6403405148ab6b8c6cb09b1993b8
wesleyara <wes1gom@gmail.com>
2022-04-15 11:18:36 -0300

    feat: Adicionando pasta 003

---
+++ 005_MySQL_Node/003_Recebendo_dados/index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const mysql = require("mysql");
+const exphbs = require("express-handlebars");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.post("/books/insertbook", (req, res) => {
+  const title = req.body.title;
+  const pageqty = req.body.pageqty;
+
+  const query = `INSERT INTO books (title, pageqty) VALUES ('${title}', ${pageqty})`;
+
+  comn.query(query, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.redirect("/books");
+  });
+});
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/books", (req, res) => {
+  const query = "SELECT * FROM books";
+
+  comn.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.render("books", { books: results });
+  });
+});
+
+const comn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodemysql",
+});
+
+comn.connect((err) => {
+  if (err) {
+    console.log("Error connecting to Db");
+    return;
+  }
+
+  console.log("Connection established");
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000");
+  });
+});
